11b95cb75868026649133e4edec5f62af9f4bf85
J-K. Solbakken <jksolbakken@gmail.com>
2024-01-15 15:27:59 +0100

    added component from design system

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <h1>Hello, NAV!</h1>
-}
+import "@navikt/ds-css";
+import { useRef, useState } from "react";
+import { Button, Popover } from "@navikt/ds-react";
+
+export default () => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const [openState, setOpenState] = useState(false);
+
+  return (
+    <>
+      <Button
+        ref={buttonRef}
+        onClick={() => setOpenState(!openState)}
+        aria-expanded={openState}
+      >
+        Ikke klikk her!!!
+      </Button>
+
+      <Popover
+        open={openState}
+        onClose={() => setOpenState(false)}
+        anchorEl={buttonRef.current}
+      >
+        <Popover.Content>Du klarte ikke å la være, altså...</Popover.Content>
+      </Popover>
+    </>
+  );
+};
